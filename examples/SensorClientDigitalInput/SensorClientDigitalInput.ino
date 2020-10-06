54f0d9b397762c1e58742434d4fdb6d658a19b83
Erich O. Pintar <59430256+saghonfly@users.noreply.github.com>
2020-10-06 14:35:57 +0200

    Version 1.2.0
    ESP32 Support fixed

--- examples/SensorClientDigitalInput/SensorClientDigitalInput.ino
+++ examples/SensorClientDigitalInput/SensorClientDigitalInput.ino
@@ -11,13 +11,13 @@
   
 
   HOWTO Arduino IDE:
-  - Prepare two ESP8266 based devices (eg. WeMos)
+  - Prepare two ESP8266 or ESP32 based devices (eg. WeMos) or mix an ESP8266 and an ESP32 device
   - Start two separate instances of Arduino IDE and load 
     on the first one the 'SensorServer.ino' and
     on the second one the 'SensorClientDigitalInput.ino' sketch and upload 
     these to the two ESP devices.
   - Start the 'Serial Monitor' in both instances and set baud rate to 9600
-  - Type 'startpair' into the edit box of the server. Hold the pairing button on the sensor and reset the device
+  - Type 'startpair' into the edit box of the SensorServer. Hold the pairing button on the SensorClientDigitalInput device and reset the device
   - After server and client are paired, you can change the sleep time of the client in the server
     by typing 'settimeout <seconds>' into the serial terminal. 
     This will than be send next time when sensor is up.
@@ -26,7 +26,7 @@
 
   Created 11 Mai 2020
   By Erich O. Pintar
-  Modified 19 Jun 2020
+  Modified 06 Oct 2020
   By Erich O. Pintar
 
   https://github.com/saghonfly/SimpleEspNowConnection
@@ -34,16 +34,18 @@
 */
 #include "SimpleEspNowConnection.h"
 
+#include <EEPROM.h>
+
 SimpleEspNowConnection simpleEspConnection(SimpleEspNowRole::CLIENT);
 
 int pairingButton = 13;
+//int pairingButton = 27;
 int sensorButton = 12;
 bool pairingMode = false;
 
 String inputString;
-String serverAddress;
-
-int timeout = 10;
+uint8_t serverAddress[6];
+uint8_t timeout = 10;
 
 void OnSendError(uint8_t* ad)
 {
@@ -63,7 +65,7 @@ void OnMessage(uint8_t* ad, const uint8_t* message, size_t len)
 
 void OnNewGatewayAddress(uint8_t *ga, String ad)
 {  
-  serverAddress = ad;
+  memcpy(serverAddress, ga, 6);
 
   simpleEspConnection.setServerMac(ga);
 
@@ -76,34 +78,32 @@ void OnNewGatewayAddress(uint8_t *ga, String ad)
 
 bool readConfig()
 {
-    if (!SPIFFS.exists("/setup.txt")) 
-      return false;
+  for(int i=0;i<6;i++)
+  {
+    serverAddress[i] = EEPROM.read(i);
+  }
 
-    File configFile = SPIFFS.open("/setup.txt", "r");
-    if (!configFile) 
-      return false;
-
-    for(int i=0;i<12;i++) //Read server address
-      serverAddress += (char)configFile.read();
-
-    timeout = configFile.read();    
-    configFile.close();
-
-    return true;
+  timeout = EEPROM.read(6);
+  return true;
 }
 
 bool writeConfig()
 {
-    File configFile = SPIFFS.open("/setup.txt", "w");
-    if (!configFile) 
-      return false;
+  Serial.printf("Write %02X%02X%02X%02X%02X%02X to EEPROM\n", serverAddress[0],
+                                                   serverAddress[1],
+                                                   serverAddress[2],
+                                                   serverAddress[3],
+                                                   serverAddress[4],
+                                                   serverAddress[5]);
+                                                   
+  for(int i=0;i<6;i++)
+  {
+    EEPROM.write(i,serverAddress[i]);
+  }
+  EEPROM.write(6,timeout);
+  EEPROM.commit();
 
-    configFile.print(serverAddress.c_str());
-    configFile.write(timeout);
-
-    configFile.close();
-
-    return true;
+  return true;
 }
     
 void setup() 
@@ -111,9 +111,7 @@ void setup()
   Serial.begin(9600);
   Serial.println();
 
-  // start SPIFFS file system. Ensure, sketch is uploaded with FS support !!!
-  if(!SPIFFS.begin())
-    SPIFFS.format();
+  EEPROM.begin(13);
   
   simpleEspConnection.begin();
   simpleEspConnection.setPairingBlinkPort(2);  
@@ -173,6 +171,11 @@ void loop()
 
   Serial.printf("Going to sleep...I was up for %i ms...will come back in %d seconds\n", millis(), timeout); 
 
+#if defined(ESP32)
+  esp_sleep_enable_timer_wakeup(timeout * 1000000);
+  esp_deep_sleep_start();
+#else
   ESP.deepSleep(timeout * 1000000, RF_NO_CAL); // deep sleep for 10 seconds
-  delay(100);          
+  delay(100);
+#endif          
 }
